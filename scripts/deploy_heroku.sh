cbf2805f9754ac428054017fdcc31bf928416714
hoatle <hoatle@teracy.com>
2017-07-25 18:59:43 +0700

    @ #1 | use Heroku CLI to create heroku app dynamically

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -2,6 +2,25 @@
 
 set -e
 
-docker pull $DOCKER_IMAGE
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_EMAIL
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_EMAIL
+  password $HEROKU_API_KEY
+EOF
+
+chmod 600 ~/.netrc # Heroku cli complains about permissions without this
+
+FOUND_APP=$(heroku apps | grep $HEROKU_APP_NAME);
+
+if [ -z "$FOUND_APP" -a "$FOUND_APP" != " " ]; then
+  echo 'app not found, creating...';
+  heroku apps:create $HEROKU_APP_NAME --no-remote
+fi
+
+heroku config:set SECRET_KEY_BASE=$SECRET_KEY_BASE -a $HEROKU_APP_NAME
+
 docker tag $DOCKER_IMAGE $HEROKU_IMAGE
 docker push $HEROKU_IMAGE

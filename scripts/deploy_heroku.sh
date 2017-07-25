ce669f8af1a75d290f98d95013eb4d80a1f30306
hoatle <hoatle@teracy.com>
2017-07-26 00:26:30 +0700

    @ #1 | update for gitlab-ci; refactoring for travis-ci

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
@@ -13,14 +12,21 @@ EOF
 
 chmod 600 ~/.netrc # Heroku cli complains about permissions without this
 
-FOUND_APP=$(heroku apps | grep $HEROKU_APP_NAME);
+heroku whoami
+
+echo "finding $HEROKU_APP_NAME...";
+
+FOUND_APP=`heroku apps | grep $HEROKU_APP_NAME`;
+
 
 if [ -z "$FOUND_APP" -a "$FOUND_APP" != " " ]; then
-  echo 'app not found, creating...';
+  echo "$HEROKU_APP_NAME app not found, creating...";
   heroku apps:create $HEROKU_APP_NAME --no-remote
+else
+  echo "$HEROKU_APP_NAME app found, updating...";
 fi
 
-heroku config:set SECRET_KEY_BASE=$SECRET_KEY_BASE -a $HEROKU_APP_NAME
+heroku config:set SECRET_KEY_BASE=$SECRET_KEY_BASE -a $HEROKU_APP_NAME &> /dev/null # hide secret output
 
 docker tag $DOCKER_IMAGE $HEROKU_IMAGE
 docker push $HEROKU_IMAGE

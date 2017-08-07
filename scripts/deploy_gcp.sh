23bf20855a1b0d625fb78e25e8831a09a7a667ea
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2017-08-07 13:45:30 +0700

    @ #4 | should add database connection for deployment and demo: database deployment with GCP

--- scripts/deploy_gcp.sh
+++ scripts/deploy_gcp.sh
@@ -19,4 +19,9 @@ echo "helm upgrade $HELM_RELEASE_NAME --install $HELM_CHART"
 helm upgrade $HELM_RELEASE_NAME --install $HELM_CHART \
   --set replicaCount=$HELM_REPLICA_COUNT \
   --set image.repository=$HELM_IMAGE_REPOSITORY,image.tag=$HELM_IMAGE_TAG,image.pullPolicy=$HELM_IMAGE_PULL_POLICY,image.lastDeployed=$HELM_IMAGE_LAST_DEPLOYED \
-  --set app.secretKeyBase=$SECRET_KEY_BASE
+  --set app.secretKeyBase=$SECRET_KEY_BASE \
+  --set app.databaseHostProduction=$GCP_DATABASE_HOST \
+  --set app.databaseProduction=$GCP_DATABASE \
+  --set app.databaseUserProduction=$GCP_DATABASE_USER \
+  --set app.databasePortProduction=$GCP_DATABASE_PORT \
+  --set app.databasePasswordProduction=$GCP_DATABASE_PASSWORD

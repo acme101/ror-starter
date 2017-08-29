1252b69136d6a4ffd26d0cfea367e9c1bc8a4df5
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2017-08-29 18:48:47 +0700

    @ #6 | should normalize the helm release name: update

--- scripts/helm_release_name_normalization.sh
+++ scripts/helm_release_name_normalization.sh
@@ -8,6 +8,7 @@ ci_project_name=$3;
 IFS='/' read -a array <<< $img_tag;
 IFS='-' read -a array <<< ${array[-1]};
 img_tag_normalized=${array[0]};
+img_tag_normalized=${img_tag_normalized:0:5};
 if [ "${img_tag_normalized:0:1}" == "#" ]; then img_tag_normalized=${img_tag_normalized:1:1}; fi;
 for i in ${array[@]:1}; do img_tag_normalized=$img_tag_normalized-${i:0:1}; done;
 
